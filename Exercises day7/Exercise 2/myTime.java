0face007c82b79aa573b276188306f30c826e8d4
Lubna-BinSalem <110376812+Lubna-BinSalem@users.noreply.github.com>
2022-08-08 15:05:08 +0300

    Update myTime.java

--- Exercises day7/Exercise 2/myTime.java
+++ Exercises day7/Exercise 2/myTime.java
@@ -10,11 +10,11 @@ public class myTime {
     }
 
     public myTime(int hour, int minute, int second) {
-         if(hour>=0 && hour<=23){
+         if(hour>0 && hour<23){
         this.hour = hour;}
-        if(minute>=0 && minute<=59){
+        if(minute>0 && minute<59){
         this.minute = minute;}
-        if(second>=0 && second<=59){
+        if(second>0 && second<59){
         this.second = second;}
     }
     public void setTime(int hour, int minute, int second){
